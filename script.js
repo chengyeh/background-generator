b65c1e57e6714bfc27c135d151ed843105e1bad4
Cheng Yeh Lee <chengyeh@ku.edu>
2018-03-14 23:53:30 -0500

    Fix typo

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ const randomiseHex = () => {
 	const letters = '0123456789ABCDEF';
 	const color = '#';
 	for (let i = 0; i < 6; i++) {
-		olor += letters[Math.floor(Math.random() * 16)];
+		color += letters[Math.floor(Math.random() * 16)];
 	}
 	return color;
 }
