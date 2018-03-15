b21a16b42687a6fc1dd6ba428dcff8f8024bef9f
Cheng Yeh Lee <chengyeh@ku.edu>
2018-03-15 01:10:04 -0500

    Remove old veriosn function

--- script.js
+++ script.js
@@ -34,16 +34,6 @@ function randomiseRGB() {
 	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
 
-//Reference: https://stackoverflow.com/questions/1484506/random-color-generator
-// function randomiseHex() {
-// 	var letters = '0123456789ABCDEF';
-// 	var color = '#';
-// 	for (var i = 0; i < 6; i++) {
-// 		color += letters[Math.floor(Math.random() * 16)];
-// 	}
-// 	return color;
-// }
-
 const randomiseHex = () => {
 	const letters = '0123456789ABCDEF';
 	let color = '#';
